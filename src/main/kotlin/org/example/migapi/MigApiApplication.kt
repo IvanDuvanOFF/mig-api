a34c6809af757313b54207601f8f32c09d847d4a
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2024-03-03 16:06:34 +0700

    initial commit

---
+++ src/main/kotlin/org/example/migapi/MigApiApplication.kt
@@ -0,0 +1,11 @@
+package org.example.migapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MigApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MigApiApplication>(*args)
+}
