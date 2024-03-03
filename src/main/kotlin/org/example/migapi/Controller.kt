1811b9fc4c7d26bdc6b1cc3df21d8aef0c6e4b4d
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2024-03-03 18:58:42 +0700

    docker-compose set up

---
+++ src/main/kotlin/org/example/migapi/Controller.kt
@@ -0,0 +1,10 @@
+package org.example.migapi
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+    @GetMapping("/")
+    fun index() = "hello world"
+}
