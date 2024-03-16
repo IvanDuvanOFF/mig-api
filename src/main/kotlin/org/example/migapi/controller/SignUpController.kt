01505cd081baf5005b7da5d4ccda51d8fdbd7ba1
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2024-03-16 14:30:35 +0700

    added exception handler mechanism

--- src/main/kotlin/org/example/migapi/controller/SignUpController.kt
+++ src/main/kotlin/org/example/migapi/controller/SignUpController.kt
@@ -1,9 +1,16 @@
 package org.example.migapi.controller
 
+import org.example.migapi.domain.dto.AdminDto
+import org.springframework.http.HttpRequest
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/signup")
 class SignUpController {
+    @PostMapping("admin")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    fun addAdmin(adminDto: AdminDto, httpRequest: HttpRequest)
 }
