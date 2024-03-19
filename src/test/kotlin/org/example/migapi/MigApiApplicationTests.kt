501b69260558aa252cb18cb8d28a5101794a7a7c
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2024-03-19 09:23:22 +0700

    user service layer done working on controller 2

--- src/test/kotlin/org/example/migapi/MigApiApplicationTests.kt
+++ src/test/kotlin/org/example/migapi/MigApiApplicationTests.kt
@@ -1,32 +1,12 @@
 package org.example.migapi
 
-import org.example.migapi.domain.service.security.TotpService
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class MigApiApplicationTests(
-	@Autowired
-	private val totpService: TotpService
-) {
+class MigApiApplicationTests {
 
 	@Test
 	fun contextLoads() {
-	}
-
-	@Test
-	fun isCodeRandom() {
-		val secret = totpService.generateSecret()
-
-		println("SECRET = $secret")
-
-		val firstCode = totpService.generateCode(secret)
-
-		println("FIRST CODE = $firstCode")
-
-		val secendCode = totpService.generateCode(secret)
-
-		println("SECOND CODE = $secendCode")
 	}
 }
